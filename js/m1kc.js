dbf8b1ba61e6ee6bf588b0c74e4e4efdce2bd527
m1kc <m1kc@yandex.ru>
2012-07-07 11:58:13 +0400

    Updated template.

---
+++ js/m1kc.js
@@ -0,0 +1,7 @@
+function printable(sender)
+{
+	document.getElementById("leftline").style.display='none';
+	document.getElementById("leftline2").style.display='none';
+	document.getElementById("rightline").style.display='none';
+	sender.style.display='none';
+}
