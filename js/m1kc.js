3eee1e8d2f8f155ecf3e5e11540a349a4b0fd3bb
m1kc <m1kc@yandex.ru>
2012-07-09 14:45:43 +0400

    Testing hideUnseenArt().

--- js/m1kc.js
+++ js/m1kc.js
@@ -6,3 +6,14 @@ function printable()
 	
 	$('.printables').toggle();
 }
+
+function getClientHeight()
+{
+	return document.compatMode=='CSS1Compat' && !window.opera?document.documentElement.clientHeight:document.body.clientHeight;
+}
+
+function hideUnseenArt()
+{
+	alert('foo');
+	setTimeout('hideUnseenArt()', 5000);
+}
