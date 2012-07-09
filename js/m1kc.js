097a54a4ea548338821ea0c3bad5ea22892535ac
m1kc <m1kc@yandex.ru>
2012-07-09 15:55:22 +0400

    Testing unseenArt().

--- js/m1kc.js
+++ js/m1kc.js
@@ -18,16 +18,16 @@ function unseenArt()
 		///value.style.visibility = 'visible';
 		if ( $(value).position().top > ($(document).scrollTop()+$(window).height()) )
 		{
-			value.style.visibility = 'hidden';
+			//value.style.visibility = 'hidden';
 		}
 		else if ( $(value).position().top+$(value).height() < $(document).scrollTop() )
 		{
-			value.style.visibility = 'hidden';
+			//value.style.visibility = 'hidden';
 		}
 		else
 		{
 			value.src = value.dataset['src'];
-			value.style.visibility = 'visible';
+			//value.style.visibility = 'visible';
 		}
 	});
 	setTimeout('unseenArt()', 1000);
